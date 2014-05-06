82a1164b1926d471361cb86b66f48439ecd9757e
John McCaffrey <John.McCaffrey@gmail.com>
2014-05-06 14:15:44 -0500

    testing to see if we are getting loaded

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,17 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 # 
 
+module Rack
+     class JSONP
+        def pad(callback, response, body = "")
+            Rails.logger.debug("here I am!")
+          response.each{ |s| body << s.to_s }
+          close(response)
+          ["#{callback}(#{body})"]
+        end
+    end
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
