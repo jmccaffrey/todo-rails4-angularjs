ca7465b390172ef328c7e92da215471cb8d1daf1
John McCaffrey <John.McCaffrey@gmail.com>
2014-05-06 14:19:31 -0500

    more debugging

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,40 @@ require "sprockets/railtie"
 
 module Rack
      class JSONP
-        def pad(callback, response, body = "")
-            Rails.logger.debug("here I am!")
-          response.each{ |s| body << s.to_s }
-          close(response)
-          ["#{callback}(#{body})"]
-        end
+
+        def call(env)
+      # remove the callback and _ parameters BEFORE calling the backend, so
+      # that caching middleware does not store a copy for each value of the
+      # callback parameter
+      Rails.logger.debug("here I am!")
+      puts "for real"
+      request = Rack::Request.new(env)
+      callback = request.params.delete(@callback_param)
+      puts "callback #{callback}"
+      timestamp = request.params.delete(@timestamp_param)
+      env['QUERY_STRING'] = env['QUERY_STRING'].split("&").delete_if{|param|
+        param =~ /^(#{@timestamp_param}|#{@callback_param})=/
+      }.join("&")
+      env['rack.jsonp.callback'] = callback
+      env['rack.jsonp.timestamp'] = timestamp
+
+
+      puts "content type #{headers['Content-Type']}"
+      status, headers, response = @app.call(env)
+
+      if callback && headers['Content-Type'] =~ /json/i
+        response = pad(callback, response)
+        headers['Content-Length'] = response.first.bytesize.to_s
+        headers['Content-Type'] = 'application/javascript'
+      elsif @carriage_return && headers['Content-Type'] =~ /json/i
+        # add a \n after the response if this is a json (not JSONP) response
+        response = carriage_return(response)
+        headers['Content-Length'] = response.first.bytesize.to_s
+      end
+
+      [status, headers, response]
+    end
+
     end
 end
 
